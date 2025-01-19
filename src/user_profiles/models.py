e2417a7f2d49b337a397bf265ff4f6ca88eb4785
f0rw4rd-dev <52598385+f0rw4rd-dev@users.noreply.github.com>
2025-01-19 21:34:48 +0300

    Merge pull request #1
    * Create apps

---
+++ src/user_profiles/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    university_group = models.CharField(max_length=16, null=True, blank=True)
+    codeforces_handle = models.CharField(max_length=24, null=True, blank=True)
