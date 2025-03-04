7b621c68cfa52d81b8a8f4b317cecdcd6d722596
lirik1254 <kirill_shulzhik@mail.ru>
2025-03-04 17:36:03 +0500

    fix: make corrections

--- src/algopath/settings.py
+++ src/algopath/settings.py
@@ -165,8 +165,8 @@ def str_to_bool(value):
 
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
-EMAIL_USE_TLS = str_to_bool(os.environ.get('EMAIL_USE_TLS', 'false'))
-EMAIL_USE_SSL = str_to_bool(os.environ.get('EMAIL_USE_SSL', 'false'))
+EMAIL_USE_TLS = str_to_bool(os.environ.get('EMAIL_USE_TLS'))
+EMAIL_USE_SSL = str_to_bool(os.environ.get('EMAIL_USE_SSL'))
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
